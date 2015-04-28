62ff5f1b5e93d7d1fe8891fd22c6bccb80ceb19b
Guillermo Hermosillo <g.andrewhermosillo@gmail.com>
2015-04-27 21:15:40 -0700

    added pictures and finished errors

--- view/register-form.php
+++ view/register-form.php
@@ -5,7 +5,7 @@
 
 <h1>Register</h1>
 
-<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
+<form method="post" action="<?php echo $path . "todo.php"; ?>">
 	<div>
 		<label for="username">Username: </label>
 		<input type="text" name="username" />
